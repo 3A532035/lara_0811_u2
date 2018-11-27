85e126d088e9187007832908ff896a665b5da9bf
3A532035 <pp93189tw@gmail.com>
2018-11-27 11:49:16 +0800

    練習4-3

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('hello/{name?}',['as'=>'hello.index',function ($name='EveryBody'){
-    return 'Hello,'.$name;
+Route::get('say/{name?}',['as'=>'hello.index',function ($name='EveryBody'){
+    return view('welcome');
 }]);
 
